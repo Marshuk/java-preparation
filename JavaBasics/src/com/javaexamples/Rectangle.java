8f1431f554f8452ea26ccb66d3ea9c34b448bd32
Marshuk <marshuksheik@gmail.com>
2026-07-06 10:24:19 +0530

    Rectangle program

---
+++ JavaBasics/src/com/javaexamples/Rectangle.java
@@ -0,0 +1,14 @@
+package com.javaexamples;
+public class Rectangle {
+	static void calculateArea(int l, int b) {
+		int area= l*b;
+		System.out.println(area);
+
+		
+	}
+
+	public static void main(String[] args) {
+		calculateArea(20,30);
+	}
+
+}
